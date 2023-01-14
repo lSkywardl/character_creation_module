18e389ca44e233ae48fd7986f5914c738ba66cc1
Саша Тэн <11sss11sss_11@mail.ru>
2023-01-14 15:57:14 +0300

    hzh

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from random import randint
 
 from graphic_arts.start_game_banner import run_screensaver
 
+
 def attack(char_name: str, char_class: str) -> str:
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику равный '
